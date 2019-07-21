7f737120f713806eb1add6bb36969f2cbf2fe69b
Mauricio Orozco <maudob@live.com>
2019-07-21 13:03:03 -0500

    Running tests

--- src/App.js
+++ src/App.js
@@ -11,19 +11,60 @@ class App extends Component {
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: ''
+      newTask: '',
+      error : false
     }
+    this.trackInput = this.trackInput.bind(this);
+    this.addTask = this.addTask.bind(this);
   }
+
+  trackInput(event){
+    this.setState({
+      newTask : event.target.value
+    })
+  }
+
+  addTask(event){
+    event.preventDefault();
+    if(this.state.newTask.length > 1){
+      this.setState({
+        tasks : this.state.tasks.concat(
+          {
+            name : this.state.newTask,
+            done : false
+          }
+        ),
+        newTask : "",
+        error : false
+      })
+    } else {
+      this.setState({
+        error : true
+      })
+    }    
+  }
+
+  checkTask(e){
+    const index = this.state.tasks.findIndex(task => 
+      task.name === e.target.innerHTML
+    );
+    this.setState({
+      tasks: this.state.tasks.map((task, i) =>
+        i === index ? {name: task.name, done: !task.done}: task
+      )
+    })
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li className={task.done ? 'done' : ""} onClick={this.checkTask.bind(this)} key={index}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.addTask}>
+            <input type="text" id="new-task" className={this.state.error ? 'error' : ''} placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.trackInput}/>
           </form>
         </div>
       </div>
