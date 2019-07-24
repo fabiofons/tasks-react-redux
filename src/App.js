c580f8b54e0e6f0f12d19cd8055ef82d10a8b254
Mauricio Orozco <maudob@live.com>
2019-07-24 18:59:30 -0500

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
 import List from './List';
 import Form from './Form';
-/// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
 
 class App extends Component {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     tasks: [
-  //       // { id: 1, name: "Sacar la ropa", done: false },
-  //       // { id: 2, name: "Hacer la cama", done: true },
-  //       // { id: 3, name: "Leer un rato", done: false }
-  //     ],
-  //     newTask: '',
-  //     error : false
-  //   }
-
-  //   store.subscribe(() => {
-  //     this.setState({
-  //       tasks: store.getState().tasks,
-  //       newTask: store.getState().newTask
-  //     })
-  //   })    
-  // } 
 
   render() {
     return (
--- src/List.js
+++ src/List.js
@@ -1,43 +1,36 @@
 import React from 'react';
-import store from './store';
+import { connect } from 'react-redux';
 
 class List extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      tasks: [
-          { id: 1, name: "Sacar la ropa", done: false },
-          { id: 2, name: "Hacer la cama", done: true },
-      { id: 3, name: "Leer un rato", done: false }]
-    }
-
-    store.subscribe(() => {
-      this.setState({
-        tasks: store.getState().tasks
-      });
-    })
-  }
 
   render() {
     return (
     <ul className="todo">
-        {this.state.tasks.map((task, index) => <li className={task.done ? 'done' : ""} onClick={this.checkTask.bind(this)} key={index}>{task.name}</li>)}
+        {this.props.tasks.map((task, index) => <li className={task.done ? 'done' : ""} onClick={this.props.checkTask.bind(this)} key={index}>{task.name}</li>)}
     </ul>
     );
   }
-    
-  checkTask(e){
-    const index = this.state.tasks.findIndex(task => 
-      task.name === e.target.innerHTML
-    );
-    store.dispatch({
-      type: 'TOGGLE_DONE',
-      index
-    });
-  }
-
 }
 
-export default List;
+const mapStateToProps = state => {
+  return {
+    tasks: state.tasks
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    checkTask(e){
+      const index = this.props.tasks.findIndex(task => 
+        task.name === e.target.innerHTML
+      );
+      dispatch({
+        type: 'TOGGLE_DONE',
+        index
+      });
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(List);
 
